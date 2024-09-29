6c41d09cc49de83fd89b9edb92c5af81a78b693e
HoCG <rhwkd5012@naver.com>
2024-09-29 15:34:34 +0900

    design: 스타일드 컴포넌트 사용을 위힌 초기세팅

---
+++ src/styles/Theme.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './GlobalStyles';
+
+interface ITheme {
+  children: React.ReactNode;
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const colors = {
+  main: '#689ee4',
+  subMain: '#1b64d1',
+  yellow: '#FAFF00',
+  black: '#000000',
+  modernBlack: '#434343',
+  gray: '#A9A9A9',
+  white: '#FFFFFF',
+  green: '#3bb957',
+  red: '#ff3333',
+  deepSkyblue: '#adc1d9',
+};
+
+export type ColorsTypes = typeof colors;
+
+export default function Theme({ children }: ITheme) {
+  return (
+    <ThemeProvider
+      theme={{
+        colors,
+      }}
+    >
+      <GlobalStyles />
+      {children}
+    </ThemeProvider>
+  );
+}
