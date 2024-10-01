e2aeccdfc29ce914d363fb198d9d15b16c6338a9
HoCG <rhwkd5012@naver.com>
2024-10-01 23:50:52 +0900

    feat: svg 관련 설정

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,1 +1,5 @@
-export {};
+import Close1 from './close1.svg?react';
+import Close2 from './close2.svg?react';
+import Close3 from './close3.svg?react';
+
+export { Close1, Close2, Close3 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
   },
