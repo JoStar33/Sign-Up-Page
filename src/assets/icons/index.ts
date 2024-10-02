0c9ce8de780f951fa0ac44261bf789ce3e30e9d7
HoCG <rhwkd5012@naver.com>
2024-10-02 20:32:33 +0900

    design: svg 업로드

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,8 @@
 import Close1 from './close1.svg?react';
 import Close2 from './close2.svg?react';
 import Close3 from './close3.svg?react';
+import Check from './check.svg?react';
+import Heart from './heart.svg?react';
+import Robot from './robot.svg?react';
 
-export { Close1, Close2, Close3 };
+export { Close1, Close2, Close3, Check, Heart, Robot };
